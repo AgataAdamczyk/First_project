c0654468f527d2ae74c3f86b5c015ffa8cde5547
AgataAdamczyk <agata.bodakiewicz@wp.pl>
2020-06-09 16:01:18 +0200

    Custom select styled

--- js/index.js
+++ js/index.js
@@ -37,29 +37,6 @@ ordersQuantity.addEventListener("change", function(e) {
 });
 
 
-/* Select package */
-
-let calcSelect = document.querySelector(".calc__select"),
-    selectPackage = document.getElementById("package__select--option"),
-    selectPackageValue = document.getElementById("package__select--value");
-
-calcSelect.addEventListener("change", function(e) {
-
-    if(e.target.value === "basic") {
-        selectPackage.innerText = "Podstawowy";
-        selectPackageValue.innerText = "0$";
-
-    } else if (e.target.value === "professional") {
-        selectPackage.innerText = "Profesjonalny";
-        selectPackageValue.innerText = "25$";
-
-    } else if (e.target.value === "premium") {
-        selectPackage.innerText = "Premium";
-        selectPackageValue.innerText = "60$";
-    }
-});
-
-
 /* Checkbox accountant && terminal */
 
 let accountantCheckbox = document.querySelector("input[name=accountant]"),
@@ -88,23 +65,124 @@ terminalCheckbox.addEventListener("change", function() {
 });
 
 
-/* Calculate sum */
-
-let total = document.getElementById("total__value");
-
-
-// total.innerText = parseInt(productsPriceValue.value) + parseInt(ordersPriceValue) + parseInt(selectPackageValue);
-
-
 /* Select */
 
-calcSelect.addEventListener("click", function() {
+let select  = document.getElementsByClassName("calc__select");
 
-    if(this.className === "calc__select") {
 
+for(let i = 0; i < select.length; i++) {
+    
+    let selectElem = select[i].getElementsByTagName("select")[0],
+        selectInput = document.createElement("div");
+    /* For each element, create a new <div> that will act as the selected item: */
+
+    selectInput.setAttribute("class", "select-selected");
+    selectInput.innerHTML = selectElem.options[selectElem.selectedIndex].innerHTML;
+    select[i].appendChild(selectInput);
+
+    /* For each element, create a new <div> that will contain the option list: */
+    let optionList = document.createElement("div");
+    optionList.setAttribute("class", "select-items select-hide");
+
+    for(let j = 1; j < selectElem.length; j++) {
+
+    /* For each option in the original select element,
+    create a new <div> that will act as an option item: */
+        let optionItem = document.createElement("div");
+        optionItem.innerHTML = selectElem.options[j].innerHTML;
+
+        optionItem.addEventListener("click", function() {
+
+        /* When an item is clicked, update the original select box,
+        and the selected item: */
+
+            let x, y, z;
+            x = this.parentNode.parentNode.getElementsByTagName("select")[0];
+            z = this.parentNode.previousSibling;
+
+            for(let i = 0; i < x.length; i++) {
+
+                if(x.options[i].innerHTML == this.innerHTML) {
+                    x.selectedIndex = i;
+                    z.innerHTML = this.innerHTML;
+                    y = this.parentNode.getElementsByClassName("same-as-selected");
+                
+                    for(let k = 0; k < y.length; k++) {
+                        y[k].removeAttribute("class");
+                    }
+                    this.setAttribute("class", "same-as-selected");
+                    break;
+                }
+            }
+            z.click();
+
+            /* Select package and set values */
+
+            let selectPackage = document.getElementById("package__select--option"),
+                selectPackageValue = document.getElementById("package__select--value");
+
+            if(z.innerHTML === "Podstawowy") {
+                selectPackage.innerText = "Podstawowy";
+                selectPackageValue.innerText = "0$";
+        
+            } else if (z.innerHTML === "Profesjonalny") {
+                selectPackage.innerText = "Profesjonalny";
+                selectPackageValue.innerText = "25$";
+        
+            } else if (z.innerHTML === "Premium") {
+                selectPackage.innerText = "Premium";
+                selectPackageValue.innerText = "60$";
+            }
+        });
+        optionList.appendChild(optionItem);
     }
-
-});
-
+    select[i].appendChild(optionList);
 
 
+    selectInput.addEventListener("click", function(e) {
+
+    /* When the select box is clicked, close any other select boxes,
+    and open/close the current select box: */
+        e.stopPropagation();
+        closeAllSelect(this);
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("select-arrow-active");
+    });
+}
+
+function closeAllSelect(elem) {
+  
+    let selectItems = document.getElementsByClassName("select-items"),
+        selectSelected = document.getElementsByClassName("select-selected"),
+        arrNo = [];
+ 
+    for (let i = 0; i < selectSelected.length; i++) {
+      if (elem == selectSelected[i]) {
+        arrNo.push(i)
+      } else {
+        selectSelected[i].classList.remove("select-arrow-active");
+      }
+    }
+    for (i = 0; i < selectItems.length; i++) {
+      if (arrNo.indexOf(i)) {
+        selectItems[i].classList.add("select-hide");
+      } 
+    }
+  }
+
+  document.addEventListener("click", closeAllSelect);
+
+
+/* Calculate sum */
+
+
+let total = document.getElementById("total__value"),
+    productsTotal = document.getElementById("products__price--value"),
+    ordersTotal = document.getElementById("orders__price--value"),
+    packageTotal = document.getElementById("package__select--value"),
+    accountantTotal = document.getElementById("accountant__value"),
+    terminalTotal = document.getElementById("terminal__value");
+  
+total.innerText = parseInt(productsTotal.innerText) + parseInt(ordersTotal.innerText) + parseInt(packageTotal.innerText) + parseInt(accountantTotal.innerText) + parseInt(terminalTotal.innerText) + "$";
+
+// total.innerText = parseInt(productsPriceValue.value) + parseInt(ordersPriceValue) + parseInt(selectPackageValue);
